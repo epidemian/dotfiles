6756da60b8f564abbb235043b43e8a1a4282fb83
Demian Ferreiro <epidemian@gmail.com>
2014-06-04 10:52:53 -0300

    Add setup.sh to symlink dotfiles

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+DOTFILES=(.bash_profile .bashrc .gitconfig .gitignore .inputrc .profile)
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+for file in "${DOTFILES[@]}"
+do
+  ln -sfv "$DIR/$file" "$HOME/$file"
+done 
