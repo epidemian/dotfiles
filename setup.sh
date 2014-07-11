0ca94d8cdf2e5e719c7e1ad0833646c9a364f1cd
Demian Ferreiro <epidemian@gmail.com>
2014-07-11 16:40:52 -0300

    Add SHH aliases

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DOTFILES=(.bash_profile .bashrc .gitconfig .gitignore .inputrc .profile)
+DOTFILES=(.bash_profile .bashrc .gitconfig .gitignore .inputrc .profile .ssh/config)
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for file in "${DOTFILES[@]}"
