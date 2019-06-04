ae61c98f7746adcab1232145a373808d7ee60a58
Demian Ferreiro <epidemian@gmail.com>
2019-06-04 18:47:54 -0300

    Add Toptal email to gitconfig

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-DOTFILES=(.bash_aliases .bash_profile .bashrc .gitconfig .gitignore .inputrc .profile .ssh/config Gemfile)
+DOTFILES=(.bash_aliases .bash_profile .bashrc .gitconfig .gitconfig-toptal .gitignore .inputrc .profile .ssh/config Gemfile)
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for file in "${DOTFILES[@]}"
 do
   ln -sfv "$DIR/$file" "$HOME/$file"
-done 
+done
