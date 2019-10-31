ef4172aad6051eb6667628971ccc42e393337b70
Demian Ferreiro <epidemian@gmail.com>
2019-10-31 10:25:01 -0300

    Don't back up original dotfiles if they are equal to repo version

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ do
   if [ "$HOME/$file" -ef "$DIR/$file" ]; then
     echo "$HOME/$file already symlinked"
   else
-    if [ -f "$HOME/$file" ]; then
+    if [ -f "$HOME/$file" ] && ! cmp -s "$DIR/$file" "$HOME/$file"; then
       mv -v "$HOME/$file" "$HOME/$file.bak"
     fi
-    ln -sv "$DIR/$file" "$HOME/$file"
+    ln -sfv "$DIR/$file" "$HOME/$file"
   fi
 done
