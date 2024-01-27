13753ea7799a1eaea7afc3958f9f17bcc38c6d8a
Demian Ferreiro <epidemian@gmail.com>
2024-01-27 03:28:12 -0300

    Copy stuff from .bashrc to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,52 @@
-if status is-interactive
-    set -gx --prepend PATH ~/.cargo/bin
-    # Initialize zoxide as `j` and `ji`
-    zoxide init fish --cmd j --no-aliases | source
+if not status is-interactive
+    exit
 end
+
+### Abbreviations
+
+abbr -a be bundle exec
+abbr -a g git
+abbr -a gs git s
+abbr -a xo xdg-open
+# Fix dumb name for bat(1) on Ubuntu repositories.
+alias bat batcat
+
+### Misc functions
+
+# Note: these should probably be defined each on a separate file, as fish likes
+# it (see funcsave(1)).
+
+function serve-dir -d "Simple HTTP server for current directory"
+    ruby -run -ehttpd . -p8000
+end
+
+function new-script -a file -d "Create a new Bash script"
+    echo >$file "#!/bin/bash
+set -o errexit -o nounset -o pipefail
+"
+    chmod +x $file
+end
+
+function killport -a port -d "Kill any process listening on a given port"
+    lsof -t -i :$port -s TCP:LISTEN | xargs -r kill
+end
+
+function gay -d "Colorful man(1) alternative"
+    COLUMNS=(math $COLUMNS / 4) man $argv | toilet --termwidth --font pagga | lolcat --force | less -R
+end
+
+function bot -d "Retro man(1) alternative"
+    COLUMNS=(math $COLUMNS / 3) man $argv | toilet --termwidth --font future | lolcat --force --freq 0.01 --seed 540 | less -R
+end
+
+function rot13 -d "Powerful anti-spoilers cipher"
+    tr A-Za-z N-ZA-Mn-za-m
+end
+
+### PATH & tool initialization
+
+# Initialize Rust's cargo
+set -gx --prepend PATH ~/.cargo/bin
+
+# Initialize zoxide as `j` and `ji`
+zoxide init fish --cmd j --no-aliases | source
